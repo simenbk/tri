0b26904f13eb96ffe7f27a4e36411f71e60588e3
Simen Kristiansen <simenbolstad@gmail.com>
2025-12-26 00:40:35 +0100

    add write items to file

--- todo/todo.go
+++ todo/todo.go
@@ -1,10 +1,22 @@
 package todo
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Item struct {
 	Text string
 }
 
 func SaveItems(filename string, items []Item) error {
+	b, err := json.Marshal(items)
+	err = os.WriteFile(filename, b, 0644)
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
 	// Placeholder implementation
 	return nil
 }
