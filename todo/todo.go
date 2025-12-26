1daa201f59ac31d2b3344b623c8357413ef164c7
Simen Kristiansen <simenbolstad@gmail.com>
2025-12-26 16:03:27 +0100

    add priority tracking

--- todo/todo.go
+++ todo/todo.go
@@ -8,6 +8,7 @@ import (
 
 type Item struct {
 	Text string
+	Priority int
 }
 
 func SaveItems(filename string, items []Item) error {
@@ -32,4 +33,15 @@ func ReadItems(filename string) ([]Item, error) {
 		return []Item{}, err
 	}
 	return items, nil
+}
+
+func (i *Item) SetPriority(pri int) {
+	switch pri {
+	case 1:
+		i.Priority = 1
+	case 3:
+		i.Priority = 3
+	default:
+		i.Priority = 2
+	}
 }
